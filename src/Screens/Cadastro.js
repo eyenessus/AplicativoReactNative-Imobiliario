d75e470d2f59b4f276d6fc66e017138a7ed357cf
Your Name <you@example.com>
2022-08-28 14:14:00 -0300

    atualizacaoErrocamera

--- src/Screens/Cadastro.js
+++ src/Screens/Cadastro.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text, TouchableOpacity, ImageBackground, StyleSheet, TextInput } from 'react-native';
 import Database from '../database/Database';
-import { RNCamera } from 'react-native-camera';
+import { RNCamera  } from 'react-native-camera';
 import ImovelBaseConstru from '../Models/ImovelBaseConstru';
+import { ScrollView } from 'react-native-gesture-handler';
 const imagenzinha = require('../Imagens/negocio.jpeg');
+const PendingView = () => (
+  <View
+    style={{
+      flex: 1,
+      backgroundColor: 'lightgreen',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    <Text>Waiting</Text>
+  </View>
+);
 export default class Cadastro extends Component {
     constructor(props) {
         super(props);
@@ -15,27 +28,36 @@ export default class Cadastro extends Component {
             descricao: 'SEM DESCRICAO NO MOMENTO',
         }
     }
+   
     CadastraBanco = function (endereco, finalidade, tipoimovel, fotoimovel, descricao) {
         const banco = new Database();
         const imovelobjet = new ImovelBaseConstru(endereco, finalidade, tipoimovel, fotoimovel, descricao);
         banco.AdicionarImovel(imovelobjet);
     };
-    takePicture = async () => {
-        if (this.camera) {
-            const options = { quality: 0.5, base64: true };
-            const data = await this.camera.takePictureAsync(options);
-            console.log(data.uri);
-        }
-    };
+   
     render() {
+
+      takePicture = async function(camera) {
+        const options = { quality: 0.5, base64: true };
+        const data = await camera.takePictureAsync(options);
+        //  eslint-disable-next-line
+        console.log(data.uri);
+      };
         return (
             //endereço,tipofinalidade, tipoimovel, foto imovel, descricao
-            <ImageBackground source={imagenzinha} resizeMode="cover" style={{ height: '100%' }}>
-                <View style={{ alignItems: 'center', height: 610 }}>
-
+            <ScrollView>
+<ImageBackground source={imagenzinha} resizeMode="cover" style={{ height: '100%' }}>
+                <View style={{ alignItems: 'center', height: 610 }}> 
                 
-                <View style={styles.container}>
+                    <Text style={{ fontSize: 30, color: 'white' }}>Formulário para cadastro</Text>
+                    <TextInput style={{ borderWidth: 1, borderRadius: 20, alignItems: 'center', backgroundColor: 'white', borderColor: 'black', borderRadius: 20, margin: 7 }} onChangeText={(valor) => this.setState({ endereco: valor })} placeholder="Endereço do imovel" />
+                    <TextInput style={{ borderWidth: 1, borderRadius: 20, alignItems: 'center', backgroundColor: 'white', borderColor: 'black', margin: 7 }} onChangeText={(valor) => this.setState({ finalidade: valor })} placeholder="Qual tipo de finalidade (venda ou aluguel)" />
+                    <TextInput style={{ borderWidth: 1, borderRadius: 20, alignItems: 'center', backgroundColor: 'white', borderColor: 'black', margin: 7 }} onChangeText={(valor) => this.setState({ tipoimovel: valor })} placeholder="Qual tipo de imovel (casa, apartamento, comercio)" />
+                    <TextInput style={{ borderWidth: 1, borderRadius: 20, alignItems: 'center', backgroundColor: 'white', borderColor: 'black', margin: 7 }} onChangeText={(valor) => this.setState({ fotoimovel: valor })} placeholder="Foto do imovel" />
+                    <TextInput style={{ borderWidth: 1, borderRadius: 20, alignItems: 'center', backgroundColor: 'white', borderColor: 'black', margin: 7 }} onChangeText={(valor) => this.setState({ descricao: valor })} placeholder="Descricao do imovel" />
+                    <View style={styles.container}>
         <RNCamera
+        
           style={styles.preview}
           type={RNCamera.Constants.Type.back}
           flashMode={RNCamera.Constants.FlashMode.on}
@@ -64,13 +86,6 @@ export default class Cadastro extends Component {
           }}
         </RNCamera>
       </View>
-
-                    <Text style={{ fontSize: 30, color: 'white' }}>Formulário para cadastro</Text>
-                    <TextInput style={{ borderWidth: 1, borderRadius: 20, alignItems: 'center', backgroundColor: 'white', borderColor: 'black', borderRadius: 20, margin: 7 }} onChangeText={(valor) => this.setState({ endereco: valor })} placeholder="Endereço do imovel" />
-                    <TextInput style={{ borderWidth: 1, borderRadius: 20, alignItems: 'center', backgroundColor: 'white', borderColor: 'black', margin: 7 }} onChangeText={(valor) => this.setState({ finalidade: valor })} placeholder="Qual tipo de finalidade (venda ou aluguel)" />
-                    <TextInput style={{ borderWidth: 1, borderRadius: 20, alignItems: 'center', backgroundColor: 'white', borderColor: 'black', margin: 7 }} onChangeText={(valor) => this.setState({ tipoimovel: valor })} placeholder="Qual tipo de imovel (casa, apartamento, comercio)" />
-                    <TextInput style={{ borderWidth: 1, borderRadius: 20, alignItems: 'center', backgroundColor: 'white', borderColor: 'black', margin: 7 }} onChangeText={(valor) => this.setState({ fotoimovel: valor })} placeholder="Foto do imovel" />
-                    <TextInput style={{ borderWidth: 1, borderRadius: 20, alignItems: 'center', backgroundColor: 'white', borderColor: 'black', margin: 7 }} onChangeText={(valor) => this.setState({ descricao: valor })} placeholder="Descricao do imovel" />
                     <TouchableOpacity onPress={() => this.CadastraBanco(this.state.endereco, this.state.finalidade, this.state.tipoimovel, this.state.fotoimovel, this.state.descricao)}>
                         <Text style={{ backgroundColor: 'green', textAlign: 'center', width: '100%', justifyContent: 'center', color: 'white', padding: 10, margin: 5, borderRadius: 50 }}>Cadastrar</Text>
                     </TouchableOpacity>
@@ -86,30 +101,31 @@ export default class Cadastro extends Component {
 
                 </View>
             </ImageBackground>
+            </ScrollView>
+            
 
         )
 
     }
 }
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: 'column',
-      backgroundColor: 'black',
-    },
-    preview: {
-      flex: 1,
-      justifyContent: 'flex-end',
-      alignItems: 'center',
-    },
-    capture: {
-      flex: 0,
-      backgroundColor: '#fff',
-      borderRadius: 5,
-      padding: 15,
-      paddingHorizontal: 20,
-      alignSelf: 'center',
-      margin: 20,
-    },
-  });
-  
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+});
